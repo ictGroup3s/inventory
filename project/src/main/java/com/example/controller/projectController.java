28c8b857385955d7cf6c36ab8330919fec5c4b34
SeungyeonChang <schang10@una.edu>
2025-11-21 14:36:21 +0900

    web

---
+++ project/src/main/java/com/example/controller/projectController.java
@@ -0,0 +1,28 @@
+package com.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+public class projectController {
+	
+	
+	@GetMapping("cart")
+	public String cart() {
+		return "cart";
+	}
+	
+	@GetMapping("shop")
+	public String shop() {
+		return "shop";
+		
+	}
+	
+	@GetMapping("checkout")
+	public String checkout() {
+		return "checkout";
+	}
+}
