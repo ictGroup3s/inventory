d644c7e45720b0a231c1a495a71156953a232d09
Seungyeon Chang <schang10@una.edu>
2025-11-24 13:01:29 +0900

    Add files via upload

---
+++ java/com/example/controller/projectController.java
@@ -0,0 +1,48 @@
+package com.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+public class projectController {
+	
+	
+	@GetMapping("cart")
+	public String cart() {
+		return "cart";
+	}
+	
+	@GetMapping("shop")
+	public String shop() {
+		return "shop";
+		
+	}
+	
+	@GetMapping("checkout")
+	public String checkout() {
+		return "checkout";
+	}
+	
+	@GetMapping("selectall")
+	public String selectall() {
+		return "selectall";
+	}
+	
+	@GetMapping("detail")
+	public String detail() {
+		return "detail";
+	}
+	
+	@GetMapping("register")
+	public String register() {
+		return "register";
+	}
+	
+	@GetMapping("login")
+	public String login() {
+		return "login";
+	}
+}
