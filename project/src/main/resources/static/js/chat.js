98193369d65cadea6eef64975aa09abf5a99314a
SeungyeonChang <schang10@una.edu>
2025-12-03 18:12:52 +0900

    sy

--- project/src/main/resources/static/js/chat.js
+++ project/src/main/resources/static/js/chat.js
@@ -4,7 +4,31 @@
 
 
 $(function () {
+	
+	let ws;
 
+	$("#chat-open").click(function () {
+	    $("#chat-box").show();
+
+	    ws = new WebSocket("ws://localhost:8080/ws/chat?userId=user01&adminId=admin01");
+
+	    ws.onmessage = (e) => {
+	        $("#chat-messages").append("<div>" + e.data + "</div>");
+	    };
+	});
+
+	$("#chat-close").click(function () {
+	    $("#chat-box").hide();
+	    if (ws) ws.close();
+	});
+
+	$("#chat-send").click(function () {
+	    let msg = $("#chat-text").val();
+	    ws.send(msg);
+	    $("#chat-text").val("");
+	});
+
+/*
     $("#chat-icon").click(function () {
         $("#chat-box").toggle();
     });
@@ -22,5 +46,5 @@ $(function () {
         let text = $("#chat-input").val();
         ws.send(text);
         $("#chat-input").val("");
-    });
+    });*/
 });
