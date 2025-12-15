a3b980fc55b20412526be35592bb873925a8b14a
Seungyeon Chang <schang10@una.edu>
2025-12-15 17:32:04 +0900

    Update mypageController.java

--- project/src/main/java/com/example/controller/mypageController.java
+++ project/src/main/java/com/example/controller/mypageController.java
@@ -23,7 +23,7 @@ public class mypageController {
     private orderRepository orderRepository;
     
     // 주문내역 페이지
-    @GetMapping("/order/mypage")
+    @GetMapping("/order/mlist")
     public String orderList(HttpSession session, Model model) {
         try {
             // 로그인 체크
